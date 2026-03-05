9b4b456271a74e04c4a31f7cdf2aa6885a900d43
xulei <xulei292@huawei.com>
2026-03-05 21:05:00 +0800

    fix: fix aclnn compile & build issue for dispatch_ffn_combine w4a8

--- csrc/build_aclnn.sh
+++ csrc/build_aclnn.sh
@@ -48,10 +48,39 @@ elif [[ "$SOC_VERSION" =~ ^ascend910_93 ]]; then
     # for dispatch_gmm_combine_decode
     yes | cp "${HCCL_STRUCT_FILE_PATH}" "${ROOT_DIR}/csrc/utils/inc/kernel"
 
+    # for dispatch_ffn_combine_w4a8
+    SCRIPT_DIR_W4A8=$(cd "$(dirname "$0")" && pwd)
+    TARGET_DIR_W4A8="$SCRIPT_DIR_W4A8/dispatch_ffn_combine_w4_a8/op_kernel/utils/"
+    TARGET_FILE_W4A8="$TARGET_DIR_W4A8/$(basename "$HCCL_STRUCT_FILE_PATH")"
+    # for dispatch_ffn_combine
+    SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+    TARGET_DIR="$SCRIPT_DIR/dispatch_ffn_combine/op_kernel/utils/"
+    TARGET_FILE="$TARGET_DIR/$(basename "$HCCL_STRUCT_FILE_PATH")"
+    # for dispatch_ffn_combine_bf16
+    SCRIPT_DIR_BF16=$(cd "$(dirname "$0")" && pwd)
+    TARGET_DIR_BF16="$SCRIPT_DIR_BF16/dispatch_ffn_combine_bf16/op_kernel/utils/"
+    TARGET_FILE_BF16="$TARGET_DIR_BF16/$(basename "$HCCL_STRUCT_FILE_PATH")"
+
+    echo "*************************************"
+    echo $HCCL_STRUCT_FILE_PATH
+    echo "$TARGET_DIR"
+    echo "$TARGET_DIR_W4A8"
+    cp "$HCCL_STRUCT_FILE_PATH" "$TARGET_DIR_W4A8"
+    cp "$HCCL_STRUCT_FILE_PATH" "$TARGET_DIR"
+    cp "$HCCL_STRUCT_FILE_PATH" "$TARGET_DIR_BF16"
+
+    sed -i 's/struct HcclOpResParam {/struct HcclOpResParamCustom {/g' "$TARGET_FILE_W4A8"
+    sed -i 's/struct HcclRankRelationResV2 {/struct HcclRankRelationResV2Custom {/g' "$TARGET_FILE_W4A8"
+    sed -i 's/struct HcclOpResParam {/struct HcclOpResParamCustom {/g' "$TARGET_FILE"
+    sed -i 's/struct HcclRankRelationResV2 {/struct HcclRankRelationResV2Custom {/g' "$TARGET_FILE"
+    sed -i 's/struct HcclOpResParam {/struct HcclOpResParamCustom {/g' "$TARGET_FILE_BF16"
+    sed -i 's/struct HcclRankRelationResV2 {/struct HcclRankRelationResV2Custom {/g' "$TARGET_FILE_BF16"
+
     CUSTOM_OPS_ARRAY=(
         "grouped_matmul_swiglu_quant_weight_nz_tensor_list"
         "lightning_indexer_vllm"
         "sparse_flash_attention"
+        "dispatch_ffn_combine_w4_a8"
         "dispatch_ffn_combine"
         "dispatch_ffn_combine_bf16"
         "dispatch_gmm_combine_decode"
