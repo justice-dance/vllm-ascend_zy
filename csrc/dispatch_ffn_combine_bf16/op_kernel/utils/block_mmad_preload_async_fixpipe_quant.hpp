f6fe76c259ccbe23906e7724332c102e86a1b373
xulei <xulei292@huawei.com>
2026-03-06 14:43:48 +0800

    fix: resolve aclnn compile & build conflict between dispatch_ffn_combine_w4a8 and dispatch_ffn_combine

--- csrc/dispatch_ffn_combine_bf16/op_kernel/utils/block_mmad_preload_async_fixpipe_quant.hpp
+++ csrc/dispatch_ffn_combine_bf16/op_kernel/utils/block_mmad_preload_async_fixpipe_quant.hpp
@@ -89,6 +89,7 @@ public:
     using CopyGmToL1A = typename TileCopy_::CopyGmToL1A;
     using CopyGmToL1B = typename TileCopy_::CopyGmToL1B;
     using CopyGmToL1S = Gemm::Tile::CopyGmToL1<ArchTag, Gemm::GemmType<uint64_t, layout::VectorLayout>>;
+    using CopyL1ToFP = typename Gemm::Tile::QuantTileCopy<ArchTag, AType_, BType_, CType_, void, Catlass::Gemm::Tile::ScaleGranularity::PER_CHANNEL>::CopyL1ToFP;
     using CopyL1ToL0A = typename TileCopy_::CopyL1ToL0A;
     using CopyL1ToL0B = typename TileCopy_::CopyL1ToL0B;
     
@@ -157,6 +158,7 @@ public:
         ptrSoftFlagBase_ = flagPtr;
         expertPerRank_ = expertPerRank;
         InitL1(resource, l1BufAddrStart);
+        InitFpBuf(resource, FpAddrStart);
         InitL0A(resource);
         InitL0B(resource);
         InitL0C(resource);
@@ -342,6 +344,13 @@ private:
     }
 
     CATLASS_DEVICE
+    void InitFpBuf(Arch::Resource<ArchTag> &resource, uint32_t FpAddrStart)
+    {
+        uint32_t FpOffset = FpAddrStart;
+        fixpipeBuf = resource.fpBuf.template GetBufferByByte<uint64_t>(FpOffset);
+    }
+
+    CATLASS_DEVICE
     void InitL0A(Arch::Resource<ArchTag> &resource)
     {
         for (uint32_t i = 0; i < L0A_STAGES; ++i) {
@@ -466,6 +475,10 @@ private:
                 copyGmToL1S(l1STensor, params.gmBlockS, layoutTileS, layoutTileS);
                 AscendC::SetFlag<AscendC::HardEvent::MTE2_FIX>(0);
                 AscendC::WaitFlag<AscendC::HardEvent::MTE2_FIX>(0);
+
+                copyL1ToFP(fixpipeBuf, l1STensor, layoutTileS, layoutTileS);
+                AscendC::SetFlag<AscendC::HardEvent::FIX_MTE2>(0);
+                AscendC::PipeBarrier<PIPE_FIX>();
             }
             if constexpr (!ENABLE_UNIT_FLAG) {
                 AscendC::SetFlag<AscendC::HardEvent::M_FIX>(l0CEventList[l0CListId]);
@@ -497,6 +510,9 @@ private:
             #endif
         }
     }
+
+    AscendC::LocalTensor<uint64_t> fixpipeBuf;
+
     AscendC::LocalTensor<ElementA> l1ATensorList[L1_STAGES];
     AscendC::LocalTensor<ElementB> l1BTensorList[L1_STAGES];
     AscendC::LocalTensor<uint64_t> l1STensor;
